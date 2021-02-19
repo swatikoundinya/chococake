2dadc8335a90333dce474e52970d263e140b6786
Swati Koundinya <koundinyaswati@gmail.com>
2021-02-19 12:01:59 +0530

    chocolate cake recipe

---
+++ Recipe.js
@@ -0,0 +1,27 @@
+ChocolateCakeWithoutEgg;
+
+recipe(() => {
+    dryIngredients = 
+
+    1 large mixing bowl
+    1,1/2 cups of flour
+    1/4 cup of cocoa powder
+    1/2 teaspoon of salt
+    1 teaspoon of baking soda
+    mix well
+
+    wetIngredients = 
+
+    Pour 1 cup of room temp water into the dry ingredients bowl
+    1/3 cup of oil
+    1 teaspoon of vanilla extract (optional)
+    1 tablespoon of lemon juice or vinegar
+    mix gently with spatula or whisk. There should not be any bubbles in the batter
+
+    baking = 
+    pour the batter into a pan
+    bake it in a preheated oven for 25-28 minutes
+    check after 25 minutes
+    allow it to cool down for 8-10 minutes
+    decorate with different stuff andit is ready! 
+})
